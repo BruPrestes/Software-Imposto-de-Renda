33fb3b63c7b68b3e24b247eca5651077b86d11c7
Bruno <brunnoprestes@gmail.com>
2020-08-12 20:27:02 -0300

    Primeira fase do software de Calculo de imposto de renda

---
+++ src/impostorenda/Contribuinte.java
@@ -0,0 +1,37 @@
+package impostorenda;
+
+public abstract class Contribuinte {
+    
+    private String nome;
+    private double rendaBruta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getRendaBruta() {
+        return rendaBruta;
+    }
+
+    public void setRendaBruta(double rendaBruta) {
+        this.rendaBruta = rendaBruta;
+    }
+    
+    //Como calcular imposto é um método abstrato terminamos com ;
+    public abstract double calcularImposto();
+
+    @Override
+    public String toString() {
+       String s = "Nome: " + nome;
+       s += " ;Renda Bruta: " + rendaBruta;
+       return s;
+    }
+    
+    
+    
+    
+}
